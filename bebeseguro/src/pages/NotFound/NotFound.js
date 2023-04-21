5fc3357c3ce0b57c039dab36deb52068ad015da7
LucasTMB <lucas.tmb708@gmail.com>
2023-04-21 00:03:36 -0300

    Finalizando a estilização da página Not Found

--- bebeseguro/src/pages/NotFound/NotFound.js
+++ bebeseguro/src/pages/NotFound/NotFound.js
@@ -1,10 +1,28 @@
+import { NavLink } from "react-router-dom";
 import styles from "./NotFound.module.css";
 
 const NotFound = () => {
   return (
-    <div>
-        <h1>404</h1>
-        <p>Não há nada aqui :)</p>
+    <div className={styles.body}>
+      <div className={styles.container}>
+        <div className={styles.error}>
+          <h1>
+            Oh Ohh!
+          </h1>
+          <p>
+            Não conseguimos encontrar a página que você procura. Vamos voltar?
+          </p>
+          <div className={styles.btn}>
+            <NavLink
+              to="/"
+              className={styles.btn_back}
+            >
+              Voltar
+            </NavLink>
+          </div>
+        </div>
+        <img src="https://404-page-using-html-css.netlify.app/Images/404.png" alt="404 image" className={styles.error_img} />
+      </div>
     </div>
   )
 }
