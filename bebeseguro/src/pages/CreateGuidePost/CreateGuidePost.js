e3eb5c867feaf1d3271594d667ba1391f74e76c7
LucasTMB <lucas.tmb708@gmail.com>
2023-05-02 22:46:30 -0300

    Página de criação de post finalizada

--- bebeseguro/src/pages/CreateGuidePost/CreateGuidePost.js
+++ bebeseguro/src/pages/CreateGuidePost/CreateGuidePost.js
@@ -1,9 +1,20 @@
+// CSS
 import styles from "./CreateGuidePost.module.css";
 
+// hooks
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuthValue } from "../../context/AuthContext";
 import { useInsertDocument } from "../../hooks/useInsertDocument";
+
+// WYSIWYG
+import Editor from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+// bootstrap
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
 
 const CreateGuidePost = () => {
   const [title, setTitle] = useState("");
@@ -12,9 +23,9 @@ const CreateGuidePost = () => {
   const [tags, setTags] = useState([]);
   const [formError, setFormError] = useState("");
 
-  const {user} = useAuthValue();
+  const { user } = useAuthValue();
 
-  const {insertDocument, response} = useInsertDocument("guide-posts");
+  const { insertDocument, response } = useInsertDocument("guide-posts");
 
   const navigate = useNavigate();
 
@@ -26,7 +37,7 @@ const CreateGuidePost = () => {
     try {
       new URL(image);
     } catch (error) {
-      setFormError("A imagem precisa ser uma URL.")
+      setFormError("A imagem precisa ser uma URL.");
     }
 
     // criar array de tags
@@ -45,69 +56,92 @@ const CreateGuidePost = () => {
       body,
       tagsArray,
       uid: user.uid,
-      createBy: user.displayName
-    })
+      createBy: user.displayName,
+    });
 
     // redirect to home page
     navigate("/guides");
-  }
+  };
+
+  const handleEditorChange = (value) => {
+    setBody(value);
+  };
 
   return (
     <div className={styles.create_post}>
-      <h2>Criar post</h2>
-      <p>Escreva sobre o que quiser e compartilhe o seu conhecimento!</p>
-      <form onSubmit={handleSubmit}>
-        <label>
-          <span>Título:</span>
-          <input 
-            type="text" 
+      <div className={styles.page_titles}>
+        <h2>Criar post</h2>
+        <p>Escreva sobre o que quiser e compartilhe o seu conhecimento!</p>
+      </div>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="my-4" controlId="title">
+          <Form.Control
+            className="form_input"
+            type="text"
             name="title"
-            maxLength={50} 
-            required 
+            maxLength={50}
+            required
             placeholder="Pense num bom título..."
             onChange={(e) => setTitle(e.target.value)}
-            value={title} 
+            value={title}
           />
-        </label>
-        <label>
-          <span>URL da imagem:</span>
-          <input 
-            type="text" 
-            name="image" 
-            required 
-            placeholder="Insira uma imagem que representa o seu post"
+        </Form.Group>
+        <Form.Group className="my-4" controlId="image">
+          <Form.Control
+            className="form_input"
+            type="text"
+            name="image"
+            required
+            placeholder="Insira a URL de uma imagem que representa o seu post"
             onChange={(e) => setImage(e.target.value)}
-            value={image} 
+            value={image}
           />
-        </label>
-        <label>
-          <span>Conteúdo:</span>
-          <textarea 
-            name="image" 
-            required 
-            placeholder="Insira o conteúdo do post"
-            onChange={(e) => setBody(e.target.value)}
-            value={body} 
-          ></textarea>
-        </label>
-        <label>
-          <span>Tags:</span>
-          <input 
-            type="text" 
-            name="tags" 
-            required 
+        </Form.Group>
+        <Form.Group className="my-4" controlId="body" as="div">
+          <Editor
+            className={styles.editor}
+            name="body"
+            placeholder="Digite o seu conteúdo aqui..."
+            onEditorChange={handleEditorChange}
+            value={body}
+          />
+        </Form.Group>
+        <Form.Group className="my-4" controlId="tags">
+          <Form.Control
+            className="form_input"
+            type="text"
+            name="tags"
+            required
             placeholder="Insira as tags separadas por vírgula"
             onChange={(e) => setTags(e.target.value)}
-            value={tags} 
+            value={tags}
           />
-        </label>
-        {!response.loading && <button className="btn">Criar</button>}
-        {response.loading && <button className="btn" disabled>Aguarde...</button>}
-        {response.error && <p className="error">{response.error}</p>}
-        {formError && <p className="error">{formError}</p>}
-      </form>
+        </Form.Group>
+        <div className={styles.buttonsAndAlerts}>
+          {!response.loading &&
+            <Button type="submit" className={styles.createBtn}>
+              Criar
+            </Button>
+          }
+          {response.loading && (
+            <Button variant="secondary" disabled>
+              Aguarde...
+            </Button>
+          )}
+          {response.error &&
+            <Alert variant="danger">
+              {response.error}
+            </Alert>
+          }
+          {formError &&
+            <Alert variant="danger">
+              {formError}
+            </Alert>
+          }
+        </div>
+      </Form>
     </div>
-  )
-}
+  );
+};
 
-export default CreateGuidePost
+export default CreateGuidePost;
