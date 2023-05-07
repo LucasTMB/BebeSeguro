1e833ff26e77187f16036a0e3eec6c33ff75b839
LucasTMB <lucas.tmb708@gmail.com>
2023-05-07 20:53:49 -0300

    Adicionando adms

--- bebeseguro/src/hooks/useCheckAdm.js
+++ bebeseguro/src/hooks/useCheckAdm.js
@@ -14,6 +14,14 @@ export const useCheckAdm = () => {
                 setAdm(true);
             else if (user.uid === '4cXDOK9fDWSc4xFcmIXePNCt25m2')
                 setAdm(true);
+            else if (user.uid === 'S0ssykAo4PM7XOxpnH16QTEJ7na2')
+                setAdm(true);
+            else if (user.uid === '7M6LUwVXckemGHbOXWz1gy8aG783')
+                setAdm(true);
+            else if (user.uid === 'aCA8xmDvpdhffoT5KbNuyctliFm2')
+                setAdm(true);
+            else if (user.uid === 'dNnRGYY20ydaKq3OhDIVgEvqeLf1')
+                setAdm(true);
             else
                 setAdm(false);
         };
@@ -21,5 +29,5 @@ export const useCheckAdm = () => {
         checkingAdm();
     }, [user]);
 
-    return {adm};
+    return { adm };
 }
