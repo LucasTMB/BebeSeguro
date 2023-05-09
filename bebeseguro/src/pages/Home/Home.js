c7cd617833ce9d5077cb2fc0426a951115ef0243
LucasTMB <lucas.tmb708@gmail.com>
2023-05-08 22:37:22 -0300

    Fazendo o banner da home para quem não está logado

--- bebeseguro/src/pages/Home/Home.js
+++ bebeseguro/src/pages/Home/Home.js
@@ -1,8 +1,59 @@
+// CSS
 import styles from "./Home.module.css";
 
+// firebase
+import { onAuthStateChanged } from 'firebase/auth';
+
+// hooks
+import { Link } from "react-router-dom";
+import { useAuthentication } from "../../hooks/useAuthentication";
+import { useState, useEffect } from "react";
+
+// react bootstrap
+import Button from "react-bootstrap/Button";
+
 const Home = () => {
+
+  const [user, setUser] = useState(undefined);
+  const { auth } = useAuthentication();
+
+  const loadingUser = user === undefined;
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    })
+  }, [auth]);
+
+  if (loadingUser) {
+    return <p>Carregando...</p>
+  }
+
   return (
-    <div>Home</div>
+    <main>
+      {!user && (
+        <section className={styles.banner}>
+          <h1>
+            BebeSeguro
+          </h1>
+          <p>
+            Cada dia é uma nova descoberta na jornada da maternidade.
+          </p>
+          <div className={styles.buttons}>
+            <Link className={styles.loginBtn} to="/login">
+              <Button>
+                Entrar
+              </Button>
+            </Link>
+            <Link className={styles.registerBtn} to="/register">
+              <Button>
+                Cadastre-se
+              </Button>
+            </Link>
+          </div>
+        </section>
+      )}
+    </main>
   )
 }
 
