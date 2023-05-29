dfed810b70923f2c863f4068f5d7205f40b9f6d5
LucasTMB <lucas.tmb708@gmail.com>
2023-05-28 22:55:57 -0300

    Finalizando a tela de home quando o usuário não estiver logado

--- bebeseguro/frontend/src/components/HomeWithoutLogin.js
+++ bebeseguro/frontend/src/components/HomeWithoutLogin.js
@@ -3,6 +3,7 @@ import styles from "./HomeWithoutLogin.module.css";
 
 // hooks
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
 
 // images
 import Calculadora from "./images/calculadora.png";
@@ -10,12 +11,26 @@ import Blog from "./images/blog.png";
 import Comunidade from "./images/comunidade.png";
 import Loja from "./images/loja.png";
 
+// scroll reveal
+import ScrollReveal from "scrollreveal";
+
 // react bootstrap
 import Button from "react-bootstrap/Button";
 
 const HomeWithoutLogin = () => {
+
+    useEffect(() => {
+        ScrollReveal().reveal(".reveal", {
+            delay: 200,
+            duration: 800,
+            distance: "20px",
+            easing: "ease-out",
+            origin: "bottom",
+        });
+    }, []);
+
     return (
-        <div>
+        <div className={styles.HomeWithoutLogin}>
 
             <section className={styles.banner}>
                 <h1>
@@ -38,7 +53,7 @@ const HomeWithoutLogin = () => {
                 </div>
             </section>
             <section className={styles.welcome}>
-                <div className={styles.texts}>
+                <div className={`${styles.texts} reveal`}>
                     <h2>
                         Bem-vinda ao nosso site para mães!
                     </h2>
@@ -46,13 +61,13 @@ const HomeWithoutLogin = () => {
                         Nós sabemos que a maternidade pode ser uma jornada incrível, mas também desafiadora. É por isso que estamos aqui para oferecer suporte e ajudá-la em cada passo do caminho.
                     </p>
                 </div>
-                <img src="https://plus.unsplash.com/premium_photo-1675035675311-c44efb41102a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="" />
+                <img src="https://plus.unsplash.com/premium_photo-1675035675311-c44efb41102a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="Mulher grávida segurando sua barriga" />
             </section>
             <section className={styles.whyUse}>
-                <h2>
+                <h2 className="reveal">
                     Por que utilizar o BebeSeguro?
                 </h2>
-                <ul>
+                <ul className="reveal">
                     <li>
                         <img src={Calculadora} alt="Calculadora" />
                         <h3>
@@ -90,8 +105,8 @@ const HomeWithoutLogin = () => {
                         </p>
                     </li>
                 </ul>
-                <div>
-                    <div>
+                <div className={`${styles.textDiv} reveal`}>
+                    <div className={styles.whiteText}>
                         <p>
                             Navegue pelo nosso site para descobrir mais sobre nossos recursos, ler os artigos do nosso blog, fazer compras em nossa loja e participar da nossa comunidade virtual.
                         </p>
