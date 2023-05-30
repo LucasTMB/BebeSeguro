06b0a2e78cfb87598b80ccb1f23dcd04ff0c601b
LucasTMB <lucas.tmb708@gmail.com>
2023-05-29 23:59:17 -0300

    Adicionando a logo ao projeto e estilizando o Header e Footer

--- bebeseguro/frontend/src/components/Footer.js
+++ bebeseguro/frontend/src/components/Footer.js
@@ -1,10 +1,40 @@
 import styles from "./Footer.module.css";
 
+// hooks
+import { Link } from "react-router-dom";
+
+// images
+import Logo from './images/BebeSeguro.png';
+
+// react icons
+import { 
+  BsFacebook,
+  BsTwitter,
+  BsInstagram
+} from 'react-icons/bs';
+
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <h3>Conectando mães com outras mães!</h3>
-      <p>BebeSeguro &copy; 2023</p>
+      <section className={styles.container}>
+        <div className={styles.brand}>
+          <img src={Logo} alt="Logo da BebeSeguro" />
+          <p>
+            &copy; 2023 BebeSeguro, Inc
+          </p>
+        </div>
+        <div className={styles.socialMedias}>
+          <a href="">
+            <BsTwitter className={styles.apps} />
+          </a>
+          <a href="">
+            <BsInstagram className={styles.apps} />
+          </a>
+          <a href="">
+            <BsFacebook className={styles.apps} />
+          </a>
+        </div>
+      </section>
     </footer>
   )
 }
--- bebeseguro/frontend/src/components/Header.js
+++ bebeseguro/frontend/src/components/Header.js
@@ -1,15 +1,13 @@
 // hooks
 import { NavLink, useNavigate } from "react-router-dom";
-//import { useAuthentication } from "../hooks/useAuthentication";
-import { useState } from "react";
 import { useAuth } from "../hooks/useAuth";
 import { useDispatch, useSelector } from "react-redux";
 
-// context
-//import { useAuthValue } from "../context/AuthContext";
-
 // styles
 import styles from "./Header.module.css";
+
+// images
+import Logo from './images/BebeSeguro.png';
 
 // redux
 import { logout, reset } from "../slices/authSlice";
@@ -18,10 +16,10 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import Button from "react-bootstrap/Button";
 
 const Header = () => {
-  //const { user } = useAuthValue();
-  //const { logout } = useAuthentication();
   const { auth } = useAuth();
   const { user } = useSelector((state) => state.auth);
 
@@ -37,96 +35,77 @@ const Header = () => {
   }
 
   return (
-    <Navbar fixed="top" bg="light" expand="lg" className={styles.navbar}>
-      <Container className={styles.containerNavbar}>
-        <NavLink
-          to="/"
-          className={`${"navbar-brand"} ${styles.brand}`}
+    <Navbar fixed="top" bg="light" expand="lg" className={`${styles.navbar} mb-3`}>
+      <Container fluid>
+        <Navbar.Brand href="/" className={styles.brand}>
+          <img src={Logo} alt="Logo da BebeSeguro" />
+          <h3>
+            Bebe<span>Seguro</span>
+          </h3>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-lg`} />
+        <Navbar.Offcanvas
+          id={`offcanvasNavbar-expand-lg`}
+          aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
+          placement="end"
         >
-          Bebe<span>Seguro</span>
-        </NavLink>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <NavLink
-              to="/"
-              exact className="nav-link"
-            >
-              Home
-            </NavLink>
-            <NavLink
-              to="/store"
-              className="nav-link"
-            >
-              Loja
-            </NavLink>
-            {auth && user && (
-              <>
-                <NavLink
-                  to="/guides"
-                  className="nav-link"
-                >
-                  Blog
-                </NavLink>
-                <NavLink
-                  to="/community"
-                  className="nav-link"
-                >
-                  Comunidade
-                </NavLink>
-              </>
-            )}
-            <NavDropdown title="Calculadoras" id="basic-nav-dropdown">
-              <NavLink to="/calculators/ovulation" className="nav-link">
-                Período fértil
-              </NavLink>
-              <NavLink to="/calculators/gestage" className="nav-link">
-                Idade gestacional
-              </NavLink>
-              <NavLink to="/calculators/imc" className="nav-link">
-                IMC para gestantes
-              </NavLink>
-              <NavLink to="/calculators/deliverydate" className="nav-link">
-                Data do parto
-              </NavLink>
-            </NavDropdown>
-            <NavLink
-              to="/about"
-              className="nav-link"
-            >
-              Sobre Nós
-            </NavLink>
-            {!auth && (
-              <>
-                <NavLink
-                  to="/login"
-                  className="nav-link"
-                >
-                  Entrar
-                </NavLink>
-                <NavLink
-                  to="/register"
-                  className="nav-link"
-                >
-                  Cadastre-se
-                </NavLink>
-              </>
-            )}
-            {auth && user && (
-              <>
-                <NavLink
-                  to={`/users/${user._id}`}
-                  className="nav-link"
-                >
-                  Perfil
-                </NavLink>
-                <button onClick={handleLogout}>Sair</button>
-              </>
-            )}
-          </Nav>
-        </Navbar.Collapse>
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-lg`}>
+              BebeSeguro
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="justify-content-end flex-grow-1 pe-3">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/store">Loja</Nav.Link>
+              {auth && user && (
+                <>
+                  <Nav.Link href="/guides">Blog</Nav.Link>
+                  <Nav.Link href="/community">Comunidade</Nav.Link>
+                </>
+              )}
+              <NavDropdown
+                title="Calculadoras"
+                id={`offcanvasNavbarDropdown-expand-lg`}
+              >
+                <NavDropdown.Item href="/calculators/ovulation">Período fértil</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="/calculators/gestage">
+                  Idade gestacional
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="/calculators/imc">
+                  IMC para gestantes
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="/calculators/deliverydate">
+                  Data do parto
+                </NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="/about">Sobre Nós</Nav.Link>
+              {!auth && (
+                <>
+                  <Nav.Link href="/login">Entrar</Nav.Link>
+                  <Nav.Link href="/register">Cadastre-se</Nav.Link>
+                </>
+              )}
+              {auth && user && (
+                <>
+                  <Nav.Link href={`/users/${user._id}`}>Perfil</Nav.Link>
+                  <Button 
+                    variant="outline-danger"
+                    onClick={handleLogout}
+                  >
+                    Sair
+                  </Button>
+                </>
+              )}
+            </Nav>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
       </Container>
     </Navbar>
+
   )
 }
 
